908f84aba7af22015a37fff361e8698398d4769c
cmsxi <cms0755@naver.com>
2024-04-17 11:21:29 +0900

    Add [백준] 11720번 숫자의 합

--- src/baekjoon/최민서/숫자의합_11720번.java
+++ src/baekjoon/최민서/숫자의합_11720번.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class 숫자의합_11720번 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
