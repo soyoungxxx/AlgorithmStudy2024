fcff11f72f69ba507a7d67a6b78deb8609d54a05
soyoungxxx <otherplet@gmail.com>
2024-04-17 11:31:28 +0900

    feat: Add [백준] 11720 숫자의 합

--- src/baekjoon/하소영/숫자의합_11720.java
+++ src/baekjoon/하소영/숫자의합_11720.java
@@ -1,4 +1,17 @@
 package baekjoon.하소영;
 
+import java.util.Scanner;
+
 public class 숫자의합_11720 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = Integer.parseInt(sc.nextLine());
+        int sum = 0;
+        String[] str = sc.nextLine().split("");
+
+        for (int i = 0; i < N; i++) {
+            sum += Integer.parseInt(str[i]);
+        }
+        System.out.println(sum);
+    }
 }
